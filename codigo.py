39add2905b99426a5400cc309ac10a5aa64d1995
Sebastián Romero Moreno <sebrommor@alum.us.es>
2025-06-11 23:05:29 +0200

    comit ya actualizado, está comentado

--- codigo.py
+++ codigo.py
@@ -15,7 +15,7 @@ def draw_from_json(json_file):
     with open(json_file) as f:
         regions = json.load(f)
 
-    # Calcular límites para centrar
+    
     all_points = [(p[0], p[1]) for r in regions for p in r['contour']]
     min_x = min(p[0] for p in all_points)
     max_x = max(p[0] for p in all_points)
@@ -28,7 +28,7 @@ def draw_from_json(json_file):
     center_x = (min_x + max_x) / 2
     center_y = (min_y + max_y) / 2
 
-    update_every_n_lines = 600  # Más líneas entre updates = más velocidad
+    update_every_n_lines = 600  # a más líneas entre updates = más velocidad
 
     for region in regions:
         color = '#{:02x}{:02x}{:02x}'.format(
@@ -38,12 +38,10 @@ def draw_from_json(json_file):
         )
         t.color(color, color)
 
-        points = region['contour'][::1]  # Puedes probar [::2] si quieres aún más velocidad
+        points = region['contour'][::2]  # Si pones [::2] tienes más velocidad, si pones [::1] tienes mas calidad
 
         if not points:
             continue
-
-        # t.begin_fill()  # puedes comentar esto si relleno no es esencial
         t.penup()
 
         x = (points[0][0] - center_x) * scale
@@ -60,11 +58,11 @@ def draw_from_json(json_file):
                 screen.update()
 
         t.penup()
-        # t.end_fill()
-        screen.update()  # asegúrate de ver cada región
+        t.end_fill()
+        screen.update()  
 
     screen.update()
     screen.mainloop()
 
 if __name__ == "__main__":
-    draw_from_json("archivo.json")
+    draw_from_json("sunflowers.json")
